3d8744c5700a45e750dd2c97e6b35d6f115bf716
Adhemar <adhemar.duran@fundacion-jala.org>
2022-10-25 22:59:06 -0400

    add axios dependency

--- tests/support/steps/api-steps.js
+++ tests/support/steps/api-steps.js
@@ -1,4 +1,5 @@
 const { Given, When } = require('@cucumber/cucumber');
+const ConfigurationManager = require('../../../core/utils/configuration_manager');
 
 Given("the user sets the following body:", function(dataTable) {
     this.requestBody = dataTable.rowsHash();
