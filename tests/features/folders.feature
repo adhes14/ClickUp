0b7b89935d02d0148688192eef776a7ec644428f
Adhemar <adhemar.duran@fundacion-jala.org>
2022-10-26 15:33:18 -0400

    fix documentation comments in steps file

--- tests/features/folders.feature
+++ tests/features/folders.feature
@@ -3,14 +3,10 @@ Feature: Folders
     Defines scenarios for Folders feature
 
     Scenario: Verify a new folder can be created (CA-03)
-        # Sets a body object for an API request
         Given the user sets the following body:
             | name | New Folder |
-        # Sets type of user, verb type and the endpoint of the request
         When the "owner" user sends a "POST" request to "/space/55613801/folder" endpoint
-        # Defines the response status
         Then the response status code should be 200
-        # Sets a 2 dimension array for a response
         And the response body should have the following values:
             | name              | New Folder |
             | override_statuses | false      |
--- tests/support/steps/api-steps.js
+++ tests/support/steps/api-steps.js
@@ -3,10 +3,16 @@ const axios = require('axios');
 const { expect } = require('expect');
 const ConfigurationManager = require('../../../core/utils/configuration_manager');
 
+/**
+ * Sets a body object for an API request
+ */
 Given("the user sets the following body:", function(dataTable) {
     this.requestBody = dataTable.rowsHash();
 });
 
+/**
+ * Sets type of user, verb type and the endpoint of the request
+ */
 When("the {string} user sends a {string} request to {string} endpoint", async function(user, verb, endpoint) {
     const header = ConfigurationManager.environment.users[user];
     const options = {
@@ -18,11 +24,17 @@ When("the {string} user sends a {string} request to {string} endpoint", async fu
     this.response = await axios.request(options);
 });
 
+/**
+ * Verify if the response code status is the same as expected
+ */
 Then("the response status code should be {int}", function (expectedCodeStatus) {
     console.log(this.response.data, expectedCodeStatus);
     expect(this.response.status).toBe(expectedCodeStatus);
 });
 
+/**
+ * Sets a 2 dimension array for a response, and verify one by one if it is the same as expected
+ */
 Then("the response body should have the following values:", function (table) {
     const tableValues = table.raw();
     for (let index = 0; index < tableValues.length; index++) {
