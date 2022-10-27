6f93f14af8391cfc297e96bc7d271fc1401768d5
Adhemar <adhemar.duran@fundacion-jala.org>
2022-10-27 11:19:37 -0400

    add delete folder hook

---
+++ tests/support/hooks/api_hooks.js
@@ -0,0 +1,16 @@
+const { Before, After } = require('@cucumber/cucumber');
+const ConfigurationManager = require('../../../core/utils/configuration_manager');
+const axios = require('axios');
+
+After({ tags: "@deleteFolder" }, async function () {
+    console.log('Deleting folder hook...');
+    const header = ConfigurationManager.environment.users['owner'];
+    const options = {
+        url: `${ConfigurationManager.environment.apiUrl}/folder/${this.response.data.id}`,
+        method: 'DELETE',
+        headers: header,
+        validateStatus: undefined
+    };
+    console.log(options);
+    await axios.request(options);
+});
