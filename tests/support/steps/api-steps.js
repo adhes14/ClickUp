64b98ae2e2f921b5f422061fef1668367f484507
Jorge A Ramirez <Jorge.Ramirez@fundacion-jala.org>
2022-10-27 12:35:22 -0500

    Add Request Manager and Loggers config

--- tests/support/steps/api-steps.js
+++ tests/support/steps/api-steps.js
@@ -1,9 +1,11 @@
 const { Given, When, Then } = require('@cucumber/cucumber');
-const axios = require('axios');
 const { expect } = require('expect');
 const ConfigurationManager = require('../../../core/utils/configuration_manager');
+const RequestManager = require('../../../core/api/request_manager.js');
+const logger = require('../../../core/utils/logger_manager');
 
 Given("the user sets the following complete body:", function(dataTable) {
+    logger.info("Parsing body string to JSON...");
     this.requestBody = JSON.parse(dataTable);
 });
 
@@ -16,17 +18,11 @@ Given("the user sets the following body:", function(dataTable) {
 
 When("the {string} user sends a {string} request to {string} endpoint", async function(user, verb, endpoint) {
     const header = ConfigurationManager.environment.users[user];
-    const options = {
-        url: `${ConfigurationManager.environment.apiUrl}${endpoint}`,
-        method: verb,
-        headers: header,
-        data: this.requestBody
-    };
-    this.response = await axios.request(options);
+    this.response = await RequestManager.send(verb, endpoint, header, this.requestBody);
 });
 
 Then("the response status code should be {int}", function (expectedCodeStatus) {
-    console.log(this.response.data, expectedCodeStatus);
+    logger.debug(this.response.data, expectedCodeStatus);
     expect(this.response.status).toBe(expectedCodeStatus);
 });
 
