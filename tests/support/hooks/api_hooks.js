4a760ffc23415c86a98b18f21c81d547aed33698
Adhemar <adhemar.duran@fundacion-jala.org>
2022-10-28 14:35:16 -0400

    add create space before hook and delete it

--- tests/support/hooks/api_hooks.js
+++ tests/support/hooks/api_hooks.js
@@ -1,6 +1,9 @@
 const { Before, After } = require('@cucumber/cucumber');
 const RequestManager = require('../../../core/api/RequestManager');
 const logger = require('../../../core/utils/logger_manager');
+const { cwd } = require('process');
+const path = require('path');
+const FileReader = require('../../../core/utils/file_reader');
 
 /**
  * It deletes a folder which has been created before
@@ -16,6 +19,7 @@ After({ tags: "@deleteFolder" }, async function () {
 After ({tags: "@deleteSpace"}, async function () {
     logger.info("Delete Space hook...");
     await RequestManager.send('DELETE', `/space/${this.response.data.id}`, {}, {}, 'owner');
+    this.space.id ? await RequestManager.send('DELETE', `/space/${this.space.id}`, {}, {}, 'owner') : '';
 });
 
 /**
@@ -39,3 +43,11 @@ Before({ tags: "@getTeamId" }, async function () {
     const response = await RequestManager.send('GET', '/team', {}, {}, 'owner');
     this.teamId = response.data.teams[0].id;
 });
+
+Before({ tags: "@createSpace" }, async function () {
+    logger.info('Creating a speace...');
+    const spacePath = `${cwd()}${path.sep}main${path.sep}resources${path.sep}createSpace.json`;
+    const spaceJson = FileReader.readJson(spacePath);
+    const response = await RequestManager.send('POST', `/team/${this.teamId}/space`, {}, spaceJson, 'owner');
+    this.space = response.data;
+})
