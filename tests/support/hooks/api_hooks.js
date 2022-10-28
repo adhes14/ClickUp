07127c538c875b200f3c8af46d3a96129c5e33b7
Pablo Huanca <Pablo.huanca@fundacion-jala.org>
2022-10-28 13:51:25 -0400

    added documentation

--- tests/support/hooks/api_hooks.js
+++ tests/support/hooks/api_hooks.js
@@ -26,6 +26,9 @@ After({ tags: "@deleteGoal" }, async function () {
     await RequestManager.send('DELETE', `/goal/${this.response.data.goal.id}`, {}, {}, 'owner');
 });
 
+/**
+ * It deletes a list which has been created before
+ */
 After ({tags: "@deleteList"}, async function () {
     logger.info("Delete List hook...");
     await RequestManager.send('DELETE', `/list/${this.response.data.id}`, {}, {}, 'owner');
